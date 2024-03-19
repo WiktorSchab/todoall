d474619dfb791aab5d99e36523d534f86522b574
WiktorSchab <wiktormateuszschab@gmail.com>
2024-03-19 18:26:25 +0100

    Added ProjectMember and ProjectTable models
    Added ProjectMember and ProjectTable models. The first one is model that will holds information about which users should have access to the project. The secound one holds information about table names in project. Both models were registered in admin.

--- todoprojects/admin.py
+++ todoprojects/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, ProjectMember, ProjectTable
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('owner', 'name', 'creation_date')
     search_fields = ('owner__username', 'name')
+
+@admin.register(ProjectMember)
+class ProjectMemberAdmin(admin.ModelAdmin):
+    list_display = ('user', 'project')
+    search_fields = ('user__username', 'project__name')
+
+@admin.register(ProjectTable)
+class ProjectTableAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project')
+    search_fields = ('name', 'project__name')
--- todoprojects/models.py
+++ todoprojects/models.py
@@ -3,9 +3,23 @@ from django.contrib.auth.models import User
 
 
 class Project(models.Model):
-    owner = models.ForeignKey(User ,on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=32, blank=False)
     creation_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.owner} | {self.name}"
+        return f"{self.name}"
+    
+class ProjectMember(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.user}"
+    
+class ProjectTable(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    name = models.CharField(max_length=32, blank=False)
+
+    def __str__(self):
+        return f"{self.name}"
