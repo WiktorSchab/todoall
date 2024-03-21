8e973832e3d31a19073501a9a1030ee7090a89a6
WiktorSchab <wiktormateuszschab@gmail.com>
2024-03-21 23:23:47 +0100

    Created views for login, register, logout
    Created 3 views: "login_user", "register_user", "logout_user". Both views for login and register check if the request method is other than POST, and then an empty form is added to the context. If the method is GET, then data from the POST request is collected. The form is checked for validity. In the login view, the user is logged in if the form is valid, and in the register view, a new user is created and then logged in. Every view redirects to the home page after success.

--- core/views.py
+++ core/views.py
@@ -1,5 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .forms import LoginForm, SignUpForm
 
 def home(request):
     return render(request, 'home.html')
+
+
+def login_user(request):
+    if request.method == 'POST':  
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form,
+        'redirect_to': 'login',
+    }
+    return render(request, 'login_register.html', context)
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else: 
+        form = SignUpForm()    
+
+    context = {
+        'form':form,
+        'redirect_to': 'register',
+    }
+    return render(request, 'login_register.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
