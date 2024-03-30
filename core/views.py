42412b69efa279e9a5715d6075e64aebe559c58c
WiktorSchab <wiktormateuszschab@gmail.com>
2024-03-30 22:43:46 +0100

    Added logic for displaying private tasks
    When the user is logged in, two queries are sent to the database: one for tasks that have passed their deadlines and another for tasks with deadlines set for today or in the future. If the user is not logged in, the data is retrieved from the session. This session data is then converted into QuerySets using an "anonymous" user. The QuerySets are divided into two lists: tasks_ended and tasks, following the same logic as when the user is logged in. In both cases, the tasks are grouped by date and stored in the "grouped_tasks" dictionary. Template "mytodo.hmtl" was modified to display dynamic data.
    
    Small fixes:
    - Added min-height to the CSS style for the "single-task" class to display long descriptions properly

--- core/views.py
+++ core/views.py
@@ -4,104 +4,153 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from .forms import LoginForm, SignUpForm, PrivateTaskForm
-from .models import PrivateTask
+from .models import PrivateTask, BaseTask
+import datetime
 
 def home(request):
-    return render(request, 'home.html')
+	return render(request, 'home.html')
 
 
 def login_user(request):
-    if request.method == 'POST':  
-        form = LoginForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            messages.success(request, 'You have been successfully logged in.')
-            return redirect('home')
-    else:
-        form = LoginForm()
+	if request.method == 'POST':  
+		form = LoginForm(request, data=request.POST)
+		if form.is_valid():
+			user = form.get_user()
+			login(request, user)
+			messages.success(request, 'You have been successfully logged in.')
+			return redirect('home')
+	else:
+		form = LoginForm()
 
-    context = {
-        'form': form,
-        'redirect_to': 'login',
-    }
-    return render(request, 'login_register.html', context)
+	context = {
+		'form': form,
+		'redirect_to': 'login',
+	}
+	return render(request, 'login_register.html', context)
 
 def register_user(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Your account has been successfully created. You have been logged in.')
-            return redirect('home')
-    else: 
-        form = SignUpForm()    
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, 'Your account has been successfully created. You have been logged in.')
+			return redirect('home')
+	else: 
+		form = SignUpForm()    
 
-    context = {
-        'form':form,
-        'redirect_to': 'register',
-    }
-    return render(request, 'login_register.html', context)
+	context = {
+		'form':form,
+		'redirect_to': 'register',
+	}
+	return render(request, 'login_register.html', context)
 
 def logout_user(request):
-    logout(request)
-    messages.success(request, 'Logout was successful')
-    return redirect('home')
+	logout(request)
+	messages.success(request, 'Logout was successful')
+	return redirect('home')
 
 def mytodo(request):
-    if request.method == 'POST':
-        # Creating a form based on data submitted via POST method
-        form = PrivateTaskForm(request.POST)
+	if request.method == 'POST':
+		# Creating a form based on data submitted via POST method
+		form = PrivateTaskForm(request.POST)
 
-        if form.is_valid():
-            # Getting clean data from the form
-            title = form.cleaned_data.get('title')
-            description = form.cleaned_data.get('description')
-            date = form.cleaned_data.get('date')
-            hour = form.cleaned_data.get('hour')
-            color = form.cleaned_data.get('color')
+		if form.is_valid():
+			# Getting clean data from the form
+			title = form.cleaned_data.get('title')
+			description = form.cleaned_data.get('description')
+			date = form.cleaned_data.get('date')
+			hour = form.cleaned_data.get('hour')
+			color = form.cleaned_data.get('color')
+			done = False
 
-            # Checking if the user is logged in
-            if request.user.is_authenticated:
-                # If the user is logged in, save the form to the database
-                private_task = form.save(commit=False)
-                private_task.owner = request.user
-                private_task.save()
-            else:
-                # If the user is not logged in, save the data in the session
-                if 'private_tasks' not in request.session:
-                    request.session['private_tasks'] = [] 
+			# Checking if the user is logged in
+			if request.user.is_authenticated:
+				# If the user is logged in, save the form to the database
+				private_task = form.save(commit=False)
+				private_task.owner = request.user
+				private_task.save()
+			else:
+				# If the user is not logged in, save the data in the session
+				if 'private_tasks' not in request.session:
+					request.session['private_tasks'] = [] 
 
-                # Creating a dictionary containing task data
-                data = {
-                    'title':title,
-                    'description':description,
-                    'color':color,
-                    'date':date.isoformat(),
-                    'hour':hour.isoformat(),
-                }
+				# Creating a dictionary containing task data
+				data = {
+					'title':title,
+					'description':description,
+					'color':color,
+					'date':date.isoformat(),
+					'hour':hour.isoformat(),
+					'done':done,
+				}
 
-                # Adding task data to the session
-                request.session['private_tasks'].append(data)
+				# Adding task data to the session
+				request.session['private_tasks'].append(data)
 
-                # Marking the session as modified
-                request.session.modified = True
+				# Marking the session as modified
+				request.session.modified = True
    
-        return redirect('mytodo')
-    else:    
-        form = PrivateTaskForm()
+		return redirect('mytodo')
+	else:    
+		form = PrivateTaskForm()
 
-        # Checking if the user is logged in
-        if request.user.is_authenticated:
-            # Getting data to display from DB
-            pass
-        else:
-            # Getting data to display from Session
-            pass
+		# Checking if the user is logged in
+		if request.user.is_authenticated:
+			tasks_ended = PrivateTask.objects.filter(owner=request.user, date__lt=datetime.date.today(), done=False).order_by('date')
+			tasks = PrivateTask.objects.filter(owner=request.user, date__gte=datetime.date.today(), done=False).order_by('date')
+		else:
+			# Creating querysets with annonymous user to have same data structure when user is logged or not
+			anonymous_user = User.objects.get(username='anonymous')
+			tasks = []
+			tasks_ended = []
 
-    context = {
-        'form': form,
-    }
+			print(request.session.get('private_tasks', []))
 
-    return render(request, 'mytodo.html', context)
+			private_tasks_data = request.session.get('private_tasks', [])
+			for task_data in private_tasks_data:
+				task_date = datetime.datetime.strptime(task_data['date'], '%Y-%m-%d').date()
+
+				hour, minute, second = map(int, task_data['hour'].split(':'))
+				hour_time = datetime.time(hour=hour, minute=minute, second=second)
+
+				private_task = PrivateTask(
+					owner=anonymous_user,
+					title=task_data['title'],
+					description=task_data['description'],
+					color=task_data['color'],
+					date=task_date,
+					hour=hour_time,
+					done=task_data['done'],
+				)
+
+				if private_task.date < datetime.date.today():
+					tasks_ended.append(private_task)
+				else:
+					tasks.append(private_task)
+
+		# Simple algoritm to group tasks by date
+		grouped_tasks = {}
+		temp_tasks = []
+		prev_date = ''
+		for task in tasks:
+			if prev_date == '':
+				prev_date = task.date
+
+			if prev_date == task.date:
+				temp_tasks.append(task)
+			else:
+				grouped_tasks[prev_date] = temp_tasks
+				prev_date = task.date
+
+				temp_tasks = []
+				temp_tasks.append(task)
+		grouped_tasks[prev_date] = temp_tasks	
+
+	context = {
+		'form': form,
+		'tasks_ended':tasks_ended,
+		'grouped_tasks':grouped_tasks,
+	}
+
+	return render(request, 'mytodo.html', context)
