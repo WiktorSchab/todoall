e54ff50f205945ced95e5a23ed7c8ef360c8a46c
WiktorSchab <wiktormateuszschab@gmail.com>
2024-04-03 00:41:07 +0200

    Added new functionality with creating new project
    Enhanced functionality after creating a new project. Now, when a user creates a new project, a corresponding record is also created in the "ProjectMember" model. Both the creation of the project and the associated ProjectMember record are wrapped in a 'transaction.atomic()' context manager to ensure data integrity.

--- todoprojects/views.py
+++ todoprojects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from .models import ProjectMember
 from .forms import NewProjectForm
 
 @login_required(login_url="/login")
@@ -8,9 +10,13 @@ def todoproject(request):
 	if request.method == 'POST':
 		form = NewProjectForm(request.POST)
 		if form.is_valid():
-			project_new = form.save(commit=False)
-			project_new.owner = request.user
-			project_new.save()
+			with transaction.atomic():
+				project_new = form.save(commit=False)
+				project_new.owner = request.user
+				project_new.save()
+
+				project_member = ProjectMember(project = project_new, user=request.user)
+				project_member.save()
 		else:
 			messages.error(request, 'Error was encored. Project was not created.')
 
